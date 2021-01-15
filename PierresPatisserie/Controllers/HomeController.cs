3411bbd3a675c0639ed65501c67df55ce08864e2
cordenbrock <cordenbrock@gmail.com>
2021-01-15 15:05:20 -0800

    Add flavors and treats lists to splash page

--- PierresPatisserie/Controllers/HomeController.cs
+++ PierresPatisserie/Controllers/HomeController.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using PierresPatisserie.Models;
 
 namespace PierresPatisserie.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresPatisserieContext _db;
+    
+    
+    public HomeController(PierresPatisserieContext db)
+    {
+      _db = db;
+    }
     public ActionResult Index()
     {
+      ViewBag.Flavors =  _db.Flavors.OrderBy(f => f.FlavorType).ToList();
+      ViewBag.Treats = _db.Treats.OrderBy(t => t.TreatName).ToList();
       return View();
     }
   }
